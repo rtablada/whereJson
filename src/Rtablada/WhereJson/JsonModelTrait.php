261bd12e1ac19c51e81abc31f2ec9cc1dbe278cf
Ryan Tablada <ryan.tablada@gmail.com>
2013-12-16 15:30:45 -0600

    Added JsonModelTrait

---
+++ src/Rtablada/WhereJson/JsonModelTrait.php
@@ -0,0 +1,19 @@
+<?php namespace Rtablada\WhereJson;
+
+trait JsonModel {
+
+	/**
+	 * Get a new query builder instance for the connection.
+	 *
+	 * @return \Illuminate\Database\Query\Builder
+	 */
+	protected function newBaseQueryBuilder()
+	{
+		$conn = $this->getConnection();
+
+		$grammar = $conn->getQueryGrammar();
+
+		return new QueryBuilder($conn, $grammar, $conn->getPostProcessor());
+	}
+
+}

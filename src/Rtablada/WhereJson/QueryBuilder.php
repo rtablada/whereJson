c1959e9fa8ea1a88106319af6cf4831a0bdf40ab
Ryan Tablada <ryan.tablada@gmail.com>
2013-12-16 15:22:34 -0600

    Use as a JSON Model with custom query builder

--- src/Rtablada/WhereJson/QueryBuilder.php
+++ src/Rtablada/WhereJson/QueryBuilder.php
@@ -1,21 +1,24 @@
 <?php namespace Rtablada\WhereJson;
 
-use Illuminate\Support\ServiceProvider;
-
-trait WhereJson {
+class QueryBuilder extends \Illuminate\Database\Query\Builder {
 
 	public function whereJson($column, $columnTraverse, $operator = null, $value = null, $boolean = 'and')
 	{
-		$column = $this->buildColumn($columnTraverse);
+		$column = $this->buildColumn($column, $columnTraverse);
 
-		return $this->where($column, $operator, $value, $boolean);
+		if (!$value) {
+			$value = $operator;
+			$operator = '=';
+		}
+
+		return $this->whereRaw("{$column} {$operator} :value", compact('value'), $boolean);
 	}
 
 	protected function buildColumn($column, $columnTraverse)
 	{
 		if (is_array($columnTraverse)) {
 			foreach ($columnTraverse as $property) {
-				$column = "{$column}->'{$columnTraverse}'";
+				$column = "{$column}->'{$property}'";
 			}
 
 			return $this->replaceLastInstanceInString('->', '->>', $column);
